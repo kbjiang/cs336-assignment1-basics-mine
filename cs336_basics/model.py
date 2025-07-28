c22d2c160dc0f3348966ea79358aca230b9d28fb
kbjiang <kebei.jiang13@gmail.com>
2025-07-28 01:19:46 +0000

    minor

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -2,6 +2,7 @@ import math
 import torch
 from torch import nn
 from einops import rearrange, reduce, einsum
+from jaxtyping import Float
 
 class Linear(nn.Module):
     def __init__(
@@ -120,3 +121,213 @@ class SwiGLU(nn.Module):
             "d_model d_ff, ... d_ff -> ... d_model"
         )
         return result
+
+class RoPE(nn.Module):
+    def __init__(
+        self,
+        theta: float,
+        d_k: int,
+        max_seq_len: int,
+        device: torch.device | None = None,
+    ):
+        super().__init__()
+        self.theta = theta
+        self.d_k = d_k
+        self.max_seq_len = max_seq_len
+        self.device = device
+        rotation_matrices = []
+        for i in range(max_seq_len):
+            for k in range(d_k//2):
+                rotation_matrices.append(
+                    RoPE.get_rotation_matrix_(i, k, self.theta, self.d_k)
+                )
+        rotation_matrices = rearrange(
+            torch.tensor(rotation_matrices), 
+            "(seq_len d_model_half) row col -> seq_len d_model_half row col", 
+            seq_len = self.max_seq_len,
+        )
+        self.register_buffer("rotation_matrices", rotation_matrices, persistent=False)
+
+    @staticmethod
+    def get_rotation_matrix_(i, k, theta, d_k):
+        angle = i / theta**(2*k/d_k)
+        return [[math.cos(angle), -math.sin(angle)], [math.sin(angle), math.cos(angle)]]
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor) -> torch.Tensor:
+        rotation_matrices = self.rotation_matrices[token_positions]
+        x = rearrange(
+            x, "... seq_len (d_model_half m) -> ... seq_len d_model_half m", m = 2
+        )
+        result = einsum(
+            rotation_matrices, x,
+            "... seq_len d_model_half row col, ... seq_len d_model_half col -> ... seq_len d_model_half row"
+        )
+        result = rearrange(
+            result, "... seq_len d_model_half row -> ... seq_len (d_model_half row)"
+        )
+        return result
+
+def softmax(x:torch.Tensor, dim:int):
+    x -= torch.amax(x, dim=dim, keepdim=True)
+    x_exp = torch.exp(x)
+    return x_exp/torch.sum(x_exp, axis=dim, keepdim=True)
+
+def scaled_dot_product_attention(
+    Q: Float[torch.Tensor, " ... queries d_k"],
+    K: Float[torch.Tensor, " ... keys d_k"],
+    V: Float[torch.Tensor, " ... values d_v"],
+    mask: Float[torch.Tensor, " ... queries keys"] | None = None,
+) -> Float[torch.Tensor, " ... queries d_v"]:
+    # torch.save(Q, "q.pt")
+    # torch.save(K, "k.pt")
+    # torch.save(V, "v.pt")
+    # torch.save(mask, "mask.pt")
+    attention = einsum(
+        Q, K,
+        "... queries d_k, ... keys d_k -> ... queries keys") / math.sqrt(K.shape[-1])
+    if mask is not None:
+        attention = attention.masked_fill(~mask, -float("inf"))
+    attention = softmax(attention, -1)
+
+    result = einsum(
+        attention, V,
+        "batch_size ... queries keys, batch_size ... keys d_v-> batch_size ... queries d_v"
+    )
+    return result
+
+class multihead_self_attention(nn.Module):
+    def __init__(
+        self, d_model: int, num_heads: int, max_seq_len: int
+    ) -> torch.Tensor:
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.max_seq_len = max_seq_len
+        self.d_k = self.d_v =  self.d_model // self.num_heads
+        self.q_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.k_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.v_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.o_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.q_mha = rearrange(
+            self.q_proj_weight,
+            "(num_h d_q) d -> num_h d_q d",
+            num_h = self.num_heads
+        )
+        self.k_mha = rearrange(
+            self.k_proj_weight,
+            "(num_h d_k) d -> num_h d_k d",
+            num_h = self.num_heads
+        )
+        self.v_mha = rearrange(
+            self.v_proj_weight,
+            "(num_h d_v) d -> num_h d_v d",
+            num_h = self.num_heads
+        )
+        self.mask = ~torch.triu(torch.ones(
+            self.max_seq_len, self.max_seq_len), diagonal = 1).bool()
+        self.rope = None
+
+    def forward(self, x: torch.Tensor):
+        qx = einsum(
+            self.q_mha, x,
+            "num_h d_q d, ... seq_len d -> ... num_h seq_len d_q",
+        )
+        kx = einsum(
+            self.k_mha, x,
+            "num_h d_k d, ... seq_len d -> ... num_h seq_len d_k",
+        )
+        vx = einsum(
+            self.v_mha, x,
+            "num_h d_v d, ... seq_len d -> ... num_h seq_len d_v",
+        )
+        scaled_vx = scaled_dot_product_attention(qx, kx, vx, self.mask)
+        scaled_vx = rearrange(
+            scaled_vx,
+            "batch_size h seq_len d -> batch_size seq_len (h d)"
+        )
+        res = einsum(
+            self.o_proj_weight, scaled_vx,
+            "d_out d_in, batch_size seq_len d_in -> batch_size seq_len d_out"
+        )
+        return res
+
+class multihead_self_attention_with_rope(nn.Module):
+    def __init__(
+        self, d_model: int, num_heads: int, max_seq_len: int, theta: float
+    ) -> torch.Tensor:
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.max_seq_len = max_seq_len
+        self.theta = theta
+        self.d_k = self.d_v =  self.d_model // self.num_heads
+        self.q_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.k_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.v_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.o_proj_weight = nn.Parameter(torch.randn(self.d_model, self.d_model))
+        self.q_mha = rearrange(
+            self.q_proj_weight,
+            "(num_h d_q) d -> num_h d_q d",
+            num_h = self.num_heads
+        )
+        self.k_mha = rearrange(
+            self.k_proj_weight,
+            "(num_h d_k) d -> num_h d_k d",
+            num_h = self.num_heads
+        )
+        self.v_mha = rearrange(
+            self.v_proj_weight,
+            "(num_h d_v) d -> num_h d_v d",
+            num_h = self.num_heads
+        )
+        self.mask = ~torch.triu(torch.ones(
+            self.max_seq_len, self.max_seq_len), diagonal = 1).bool()
+        self.rope = RoPE(self.theta, self.d_k, self.max_seq_len)
+
+    def forward(self, x: torch.Tensor, token_positions: torch.Tensor = None):
+        qx = einsum(
+            self.q_mha, x,
+            "num_h d_q d, ... seq_len d -> ... num_h seq_len d_q",
+        )
+        kx = einsum(
+            self.k_mha, x,
+            "num_h d_k d, ... seq_len d -> ... num_h seq_len d_k",
+        )
+        vx = einsum(
+            self.v_mha, x,
+            "num_h d_v d, ... seq_len d -> ... num_h seq_len d_v",
+        )
+        if token_positions is None:
+            token_positions = torch.arange(qx.shape[-2]).unsqueeze(dim=0)
+        qx = self.rope(qx, token_positions)
+        kx = self.rope(kx, token_positions)
+        scaled_vx = scaled_dot_product_attention(
+            qx, kx, vx, self.mask[:token_positions.shape[-1], :token_positions.shape[-1]])
+        scaled_vx = rearrange(
+            scaled_vx,
+            "batch_size h seq_len d -> batch_size seq_len (h d)"
+        )
+        res = einsum(
+            self.o_proj_weight, scaled_vx,
+            "d_out d_in, batch_size seq_len d_in -> batch_size seq_len d_out"
+        )
+        return res
+
+class transformer_block(nn.Module):
+    def __init__(
+        self, d_model: int, num_heads: int, d_ff: int, max_seq_len: int, theta: float):
+        super().__init__()
+        self.d_model = d_model
+        self.num_heads = num_heads
+        self.d_ff = d_ff
+        self.rmsnorm1 = RMSNorm(self.d_model)
+        self.rmsnorm2 = RMSNorm(self.d_model)
+        self.attn = multihead_self_attention_with_rope(
+            d_model, num_heads, max_seq_len, theta
+        )
+        self.ffn = SwiGLU(self.d_model, self.d_ff)
+
+    def forward(self, x:torch.Tensor):
+        y = x + self.attn(self.rmsnorm1(x), token_positions=None)
+        z = y + self.ffn(self.rmsnorm2(y))
+        return z
