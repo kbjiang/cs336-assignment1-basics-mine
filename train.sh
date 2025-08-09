e608847a056a105523adbbbcb3d94a9125194e03
kbjiang <kebei.jiang13@gmail.com>
2025-08-09 15:28:56 +0000

    minor

--- train.sh
+++ train.sh
@@ -4,8 +4,8 @@
 # Make sure to set your wandb API key first: wandb login
 
 # Configuration flags - set to empty string to disable, or uncomment/comment as needed
-D_MODEL=768
-D_FF=2048
+D_MODEL=512
+D_FF=1344
 NUM_LAYERS=8
 NUM_HEADS=8
 BATCH_SIZE=64
@@ -19,10 +19,10 @@ LR_MAX=1e-3
 LR_MIN=1e-4
 # USE_WANDB="--no_wandb"              # Set to "--no_wandb" to disable wandb
 USE_WANDB=""              # Set to "--no_wandb" to disable wandb
-WANDB_PROJECT="cs336-assign1-owt" 
+WANDB_PROJECT="cs336-assign1-ts" 
 
-SUFFIX="d768-dff2048-l8-h8-cl512-bs128"
-WANDB_RUN_NAME="test-run-$SUFFIX"
+SUFFIX="d512-dff1344-l8-h8-cl512-bs128"
+WANDB_RUN_NAME="run-$SUFFIX"
 # WANDB_RUN_NAME="--wandb_run_name test-run-$(date +%Y%m%d-%H%M%S)" 
 
 # Training with wandb (default settings)
@@ -31,14 +31,14 @@ python cs336_basics/train.py \
     --d_ff $D_FF \
     --num_layers $NUM_LAYERS \
     --num_heads $NUM_HEADS \
-    --data_path data/owt_train.npy \
-    --eval_data_path data/owt_valid.npy \
-    --vocab_path data/train_bpe_vocab_owt.json \
-    --merges_path data/train_bpe_merges_owt.txt \
-    --log_path data/log-owt.jsonl \
+    --data_path data/ts_train.npy \
+    --eval_data_path data/ts_valid.npy \
+    --vocab_path data/bpe_vocab_ts.json \
+    --merges_path data/bpe_merges_ts.txt \
+    --log_path data/log-ts.jsonl \
     $WITHOUT_REPLACEMENT \
-    --checkpoint_path data/checkpoint-owt-$SUFFIX.pt \
-    --num_steps 40000 \
+    --checkpoint_path data/checkpoint-ts-$SUFFIX.pt \
+    --num_steps 10000 \
     --batch_size $BATCH_SIZE \
     --context_length $CONTEXT_LENGTH \
     --gradient_accumulation_steps $GRADIENT_ACCUMULATION_STEPS \
@@ -46,10 +46,10 @@ python cs336_basics/train.py \
     --lr $LR \
     --lr_max $LR_MAX \
     --lr_min $LR_MIN \
-    --warmup_iters 2000 \
-    --cosine_cycle_iters 40000 \
+    --warmup_iters 1000 \
+    --cosine_cycle_iters 10000 \
     --device cuda \
-    --log_interval 200 \
+    --log_interval 100 \
     $USE_WANDB \
     --wandb_project $WANDB_PROJECT \
     --wandb_run_name $WANDB_RUN_NAME \
